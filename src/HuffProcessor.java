27e9668638b9c2e1c198f79d7e30d92242e94127
Amanda Liu <al348@duke.edu>
2018-12-07 16:15:09 -0500

    ‘working’

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -75,13 +75,30 @@ public class HuffProcessor {
 	}
 
 	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
-		// TODO Auto-generated method stub
-		
+		HuffNode current = root;
+		while (true) {
+		       int bits = in.readBits(1);
+		       if (bits == -1) {
+		           throw new HuffException("bad input, no PSEUDO_EOF");
+		       }
+		       else { 
+		           if (bits == 0) current = current.myLeft;
+		           else current = current.myRight;
+
+		           if (bits == 1) {
+		               if (current.myValue == PSEUDO_EOF) break;   // out of loop
+		               else {
+		                   out.writeBits(BITS_PER_WORD, current.myValue);
+		                   current = root; // start back after leaf
+		               }
+		           }
+		       }
+		}
 	}
 
 	private HuffNode readTreeHeader(BitInputStream in) {
 		int bit = in.readBits(1);
-		if (bit ==-1) {
+		if (bit == -1) {
 			throw new HuffException("reading bits fails");
 		}
 		if (bit == 0) {
@@ -91,9 +108,9 @@ public class HuffProcessor {
 			return node;
 		}
 		else {
-			//To Do
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value, 0, null, null);
 		}
-		return null;
 	}
 
 
