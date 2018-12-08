053af621daaef11919159be07697dcada586b2f2
Amanda Liu <al348@duke.edu>
2018-12-08 10:31:34 -0500

    ‘revisited’

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -111,8 +111,16 @@
 	
 
 		private void writeHeader(HuffNode root, BitOutputStream out) {
-			out.writeBits(HUFF_NUMBER, HUFF_TREE);
-			writeTree(root,out);
+			if (root.myLeft == null && root.myRight == null) {
+				out.writeBits(1, 1);
+				out.writeBits(BITS_PER_WORD + 1, root.myValue);
+			}
+			else {
+				out.writeBits(1, 0);
+				writeHeader(root.myLeft, out);
+				writeHeader(root.myRight, out);
+			}
+
 		}
 		
 		private void writeTree(HuffNode root, BitOutputStream out) {
